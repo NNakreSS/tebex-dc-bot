dceffab4b490a67ee2d4349e829261847e783945
NNakreSS <87872407+NNakreSS@users.noreply.github.com>
2023-09-01 01:36:22 +0300

    first commit

---
+++ start-bots.js
@@ -0,0 +1,25 @@
+const { spawn } = require('child_process');
+const path = require('path');
+
+const bots = [
+  {
+    name: 'tebex-bot',
+    script: 'index.js',
+  },
+  {
+    name: 'tebex-bot-2',
+    script: 'index.js',
+  },
+];
+
+bots.forEach((bot) => {
+  const nodemonPath = path.join(__dirname, 'node_modules', '.bin', /^win/.test(process.platform) ? 'nodemon.cmd' : 'nodemon');
+  const child = spawn(nodemonPath, [bot.script], {
+    cwd: path.join(__dirname, bot.name),
+    stdio: 'inherit',
+  });
+  child.on('error', (err) => {
+    console.error(`${bot.name} botunda bir hata oluştu: ${err}`);
+  });
+  console.log(`${bot.name} botu başlatılıyor...`);
+});
